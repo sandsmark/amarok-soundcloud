3ecec5008ae97da675d8b0db57f0c437cfc5cb13
Nolaan <nolaan_d@hotmail.com>
2014-12-09 04:07:28 +0100

    Update to latest JSON API
    Soundcloud's not providing XML support from 1st December. Rewriting the app to parse JSON.

--- main.js
+++ main.js
@@ -1,60 +1,127 @@
-Importer.loadQtBinding("qt.core");
-Importer.loadQtBinding("qt.xml");
-Importer.loadQtBinding("qt.network");
-Importer.loadQtBinding("qt.gui");
+/*############################################################################*
+ *#                                                                          #*
+ *#   Amarok-Script for listening to music from SoundCloud                   #*
+ *#                                                                          #*
+ *#   Copyright (C) 2014 Sven Sperner <cethss@gmail.com>                     #*
+ *#                 2014 Nolaan <nolaan.gestionsite@gmail.com>               #*
+ *#                                                                          #*
+ *#   This program is free software; you can redistribute it and/or modify   #*
+ *#   it under the terms of the GNU General Public License as published by   #*
+ *#   the Free Software Foundation; either version 3 of the License, or      #*
+ *#   (at your option) any later version.                                    #*
+ *#                                                                          #*
+ *#   This program is distributed in the hope that it will be useful,        #*
+ *#   but WITHOUT ANY WARRANTY; without even the implied warranty of         #*
+ *#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #*
+ *#   GNU General Public License for more details.                           #*
+ *#                                                                          #*
+ *#   You should have received a copy of the GNU General Public License      #*
+ *#   along with this program. If not, look @ <http://www.gnu.org/licenses>. #*
+ *#                                                                          #*
+ *############################################################################*/
 
-function SoundCloud()
+Importer.loadQtBinding( "qt.core" );
+Importer.loadQtBinding( "qt.xml" );
+Importer.loadQtBinding( "qt.network" );
+Importer.loadQtBinding( "qt.gui" );
+
+
+
+var clientID = "?client_id=YOUR_CLIENT_ID";
+var serviceUrl = new QUrl( "http://api.soundcloud.com/tracks.json" + clientID );
+
+
+function SoundCloud( )
 {
-    ScriptableServiceScript.call(this, "SoundCloud", 2, "Search for and listen on SoundCloud.", "soundcloud.com", true);
+        ScriptableServiceScript.call( this, "SoundCloud", 1, "Stream music from SoundCloud", "http://soundcloud.com", true );
+
+        var downloader = new Downloader( serviceUrl, populateTracks );
+
 }
 
-function displayResults(results)
+function populateTracks( tracks )
 {
-    var doc = new QDomDocument;
-    doc.setContent(results);
+        // Amarok.debug( "SoundCloudScript: populateTracks:\n POPULATING STARTED with tracks : " +tracks.length );
+    // Amarok.debug( "STARTING WITH : " + tracks.substring(0,20) );
+    var textStream = new QTextStream(tracks, QIODevice.ReadOnly);
 
-    tracklist = doc.elementsByTagName("track");
+    var serviceDataJson = textStream.readAll();
 
-    for (var i=0; i<tracklist.length(); i++) {
-        try {
-            var track = tracklist.at(i); 
+    trackList = JSON.parse(tracks);
 
-            var item = Amarok.StreamItem;
+        // Amarok.debug( "SoundCloudScript: populateTracks:\n serviceDataJson: " + serviceDataJson );
+        Amarok.debug( "SoundCloudScript: populateTracks:\nFetched " + trackList.length + " tracks!" + trackList[0].user.username );
 
-            item.level = 1;
-            item.artist = track.firstChildElement("user").firstChildElement("username");
-            item.itemName = track.firstChildElement("title");
-            item.playableUrl = track.firstChildElement("stream-url");        
-            item.coverUrl = track.firstChildElement("artwork-url");
-        } catch (error) {
-            Amarok.debug(error);
+        var track;
+        var item;
+        for( var i=0; i<trackList.length; i++ )
+        {
+                try {
+                        track = trackList[i]; 
+
+                        item = Amarok.StreamItem;
+                        item.level = 0;
+                        item.itemName = track.title;
+                        item.artist = track.user.username;
+                        item.composer = track.user.username;
+                        item.album = "SoundCloud.com";
+                        item.genre = track.genre;
+                        item.year = track.created_at;
+                        item.playableUrl = track.stream_url + clientID;
+            item.coverUrl = track.artwork_url;
+            // Amarok.debug( "SoundcloudScript URL for item #" + i + " URL : " + track.stream_url);
+                        item.infoHtml = track.permalink_url;
+
+                        script.insertItem( item );
+
+                }
+                catch( error ) {
+                        Amarok.alert( error );
+                }
         }
-
-    }
-
-    script.donePopulating();
+        script.donePopulating( );
 }
 
-function onPopulate(level, callback, query)
+
+function onPopulate( level, callbackData, filter )
 {
-    if (query != "") {
-        query = query.trim().toLowerCase();
-        var url = new QUrl("http://api.soundcloud.com/tracks.xml");
-        url.addQueryItem("q", query);
-        url.addQueryItem("filter", "streamable");
-        var downloader = new Downloader(url, displayResults);
-    }
+        Amarok.debug( "SoundCloudScript: onPopulate: " + level + ",\ncallbackData: " + callbackData + ",\nfilter: " + filter );
+
+        if (filter != "")
+        {
+                filter = filter.replace( /%20/g, " " );
+                filter = filter.trim().toLowerCase( );
+
+                var queryUrl = new QUrl( serviceUrl );
+                queryUrl.addQueryItem( "q", filter );
+                queryUrl.addQueryItem( "filter", "streamable" );
+                var downloader = new Downloader( queryUrl, populateTracks );
+
+                Amarok.debug( "SoundCloudScript: onPopulate: URL: " + queryUrl );
+        }
 }
 
-function onCustomize()
+function onCustomize( )
 {
-    var cwd = Amarok.Info.scriptPath() + "/";
-    script.setIcon(new QPixmap(cwd + "soundcloud.png"));
-    script.setEmblem(new QPixmap(cwd + "soundcloud_emblem.png"));
+        Amarok.debug( "SoundCloudScript: onCustomize" );
+
+        var path = Amarok.Info.scriptPath() + "/";
+        script.setIcon( new QPixmap(path + "soundcloud_icon.png") );
+        script.setEmblem( new QPixmap(path + "soundcloud_emblem.png") );
 }
 
-script = new SoundCloud();
+function onConfigure( )
+{
+        Amarok.debug( "SoundCloudScript: onConfigure" );
 
-script.populate.connect(onPopulate);
-script.customize.connect(onCustomize);
+        //Nothing to configure by now...
+}
+
+
+
+script = new SoundCloud( );
+script.populate.connect( onPopulate );
+script.customize.connect( onCustomize );
+Amarok.configured.connect( onConfigure );
+
 
